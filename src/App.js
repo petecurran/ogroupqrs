cce5132677c4bae7d6fbb5c9ea75ea3d424315e6
Pete Curran <Curranpete@gmail.com>
2022-12-17 20:30:24 +0000

    Setting up unit selection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Layout from './pages/layout.js';
+import Setup from './pages/setup.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Setup />}/>
+              {/*<Route path="orders" element={<Orders/>} />
+              <Route path="attacking" element={<Attacking/>}/>*/}
+            </Route>
+        </Routes>
+      </div>
+  )
 }
 
 export default App;
---
+++ src/pages/layout.js
@@ -0,0 +1,44 @@
+import {Outlet, Link} from "react-router-dom"
+import { useEffect } from "react";
+
+function Layout(){
+
+    useEffect(() => {
+        // Get the navbar element
+        const navbar = document.querySelector('.navbar-collapse');
+    
+        // Add click listener to navbar links
+        document.querySelectorAll('.navbar-nav a').forEach(link => {
+          link.addEventListener('click', () => {
+            // Hide the navbar when a link is clicked
+            navbar.classList.remove('show');
+          });
+        });
+      }, []);
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                    <Link to="/" className="navbar-brand">O Group</Link>
+                    <button 
+                        className="navbar-toggler" type="button" 
+                        data-bs-toggle="collapse" data-bs-target="#mainNav" 
+                        aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="mainNav">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">Select units</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <Outlet />
+        </div>
+    )
+};
+
+export default Layout;
