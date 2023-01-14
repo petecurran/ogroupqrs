4310639710910fd85f981de5c4e031e04734adb2
petecurran <curranpete@gmail.com>
2023-01-14 17:40:25 +0000

    Artillery page 0.2

--- src/pages/artillery.js
+++ src/pages/artillery.js
@@ -29,7 +29,7 @@ const ArtilleryContainer = () => {
 
     const swapBattalion = () => {
         //swap the selected battalion
-        if (selectedBattalion == battalionOneLabel) {
+        if (selectedBattalion === battalionOneLabel) {
             setSelectedBattalion(battalionTwoLabel);
             idprefix.current = "B"
         } else {
@@ -69,7 +69,7 @@ const ArtilleryAccordion = (props) => {
 
     const handleArtillerySelect = (event) => {
         setArtillery(event.target.value);
-        if (event.target.value != "notchosen" && event.target.value != "battalion") {
+        if (event.target.value !== "notchosen" && event.target.value !== "battalion") {
             setShowArtillery("show")
         } else {
             setShowArtillery("")
@@ -107,12 +107,50 @@ const ArtilleryAccordion = (props) => {
                         </select>
                     </h4>
                     <div id={idprefix+"artillerycollapseone"} className={`accordion-collapse collapse ${showArtillery}`}  aria-labelledby={idprefix+"artilleryheadingone"} data-bs-parent={"#"+idprefix+"artilleryaccordion"}>
-                        <div className="accordion-body">
-                            <p>HELA</p>
+                        <div className="accordion-body p-0">
+                            {RollForArtillery(artillery,idprefix)}
                         </div>
-
                     </div>
-
+                </div>
+                <div className="accordion-item">
+                    <h4 className="accordion-header" id={idprefix+"artilleryheadingtwo"}>
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={"#"+idprefix+"artillerycollapsetwo"} aria-expanded="false" aria-controls={idprefix+"artillerycollapsetwo"}>
+                            Roll for accuracy
+                        </button>
+                    </h4>
+                    <div id={idprefix+"artillerycollapsetwo"} className="accordion-collapse collapse" aria-labelledby={idprefix+"artilleryheadingtwo"} data-bs-parent={"#"+idprefix+"artilleryaccordion"}>
+                        <div className="accordion-body p-0">
+                            <table className="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th colSpan={4}>Roll 2D6</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th colSpan={3}>Same aim point as last turn:</th>
+                                        <td>+1</td>
+                                    </tr>
+                                    <tr>
+                                        <th colSpan={3}>Spotter is commander or recon:</th>
+                                        <td>-1</td>
+                                    </tr>
+                                    <tr>
+                                        <th colSpan={3}>Aim point is a section at over 30":</th>
+                                        <td>-1</td>
+                                    </tr>
+                                    <tr>
+                                        <th colSpan={3}>JABO target in the open:</th>
+                                        <td>+1</td>
+                                    </tr>
+                                    <tr>
+                                        <th colSpan={3}>JABO target in buildings / woods:</th>
+                                        <td>-1</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
 
 
@@ -127,6 +165,100 @@ const ArtilleryAccordion = (props) => {
     )
 
 
+}
+
+const RollForArtillery = (artillery,idprefix) => {
+    if (artillery === "regimental") {
+        return (
+            <div>
+            <table className="table">
+                    <thead>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="text-center" colSpan={4}>
+                                Roll 2D6.<br />An artillery mission is only consumed on success.
+                            </td>
+                        </tr>
+                    </tbody>
+                    <thead>
+                        <tr>
+                            <th>Score</th>
+                            <th>Result</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>12</td>
+                            <td>Roll on the Divisional table. 7 or less = Medium battery fire mission.</td>
+                        </tr>
+                        <tr>
+                            <td>7-11</td>
+                            <td>Medium battery fire mission.</td>
+                        </tr>
+                        <tr>
+                            <td>6</td>
+                            <td>NATION SPECIFIC</td>
+                        </tr>
+                        <tr>
+                            <td>5 or less</td>
+                            <td>"No, that's map section three, not four!" No support received.</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        )
+    } else if (artillery === "divisional") {
+        return (
+            <div>
+                <table className="table">
+                    <thead>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="text-center" colSpan={4}>
+                                Roll 2D6.<br />An artillery mission is only consumed on success.
+                            </td>
+                        </tr>
+                    </tbody>
+                    <thead>
+                        <tr>
+                            <th>Score</th>
+                            <th>Result</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>12</td>
+                            <td>Jabos! 3 attacks</td>
+                        </tr>
+                        <tr>
+                            <td>10-11</td>
+                            <td>NATION SPECIFIC</td>
+                        </tr>
+                        <tr>
+                            <td>8-9</td>
+                            <td>Heavy battery fire mission</td>
+                        </tr>
+                        <tr>
+                            <td>7</td>
+                            <td>NATION SPECIFIC</td>
+                        </tr>
+                        <tr>
+                            <td>6 or less</td>
+                            <td>"It's the radios Sir... we haven't been able to make contact!" No support received.</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        )
+    } else {
+        return (
+            <div></div>
+        )
+    }
 }
 
 export default ArtilleryContainer;
